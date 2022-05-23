ff932225b5aea8096e090049c433c4b1433f54e2
Mwau stephen <stephenmwau41@gmail.com>
2022-05-23 22:47:17 +0800

    added the event handlers and states for the inputs

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -4,16 +4,27 @@ import Button from "../UI/Button";
 import styles from "./AddUser.module.css";
 
 const AddUser = () => {
+  const [enteredUserName, setEnteredUserName] = useState("");
+  const [enteredUserAge, setEnteredUserAge] = useState("");
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUserName, enteredUserAge);
   };
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUserName(event.target.value);
+  };
+  const userAgeChangeHandler = (event) => {
+    setEnteredUserAge(event.target.value);
+  };
+
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">UserName</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={userNameChangeHandler} />
         <label htmlFor="age">Age(Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={userAgeChangeHandler} />
         <Button type="submit">Add user</Button>
         <Button type="button">Delete user</Button>
       </form>
