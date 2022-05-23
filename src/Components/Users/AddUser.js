2ab122f4e8f04490b928277c41e7df34e93a14bb
Mwau stephen <stephenmwau41@gmail.com>
2022-05-23 21:16:10 +0800

    added a form on submit handler

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,8 +1,11 @@
 import React from "react";
 
 const AddUser = () => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
   return (
-    <form>
+    <form onSubmit={addUserHandler}>
       <label htmlFor="username">UserName</label>
       <input id="username" type="text" />
       <label htmlFor="age">Age(Years)</label>
