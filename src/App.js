1cc2ecb1e8c65537c50e041a945290258ee49d14
Mwau stephen <stephenmwau41@gmail.com>
2022-05-24 00:38:36 +0800

    generate the id randomly to render the lists

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React, { useState } from "react";
 import AddUser from "./Components/Users/AddUser";
 import UsersList from "./Components/Users/UsersList";
 
-function App() {
+function App(props) {
   const [usersList, setUsersList] = useState([]);
 
   // lifting state up through props
   const addUserHandler = (uName, uAge) => {
     setUsersList((prevUserList) => {
-      return [...prevUserList, { name: uName, uAge }];
+      return [
+        ...prevUserList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
     });
   };
+
   return (
     <div>
       <AddUser onAddUser={addUserHandler} />
