6a4244002c47f18f0afdf0465bcf00c0d437df6c
Mwau stephen <stephenmwau41@gmail.com>
2022-05-23 22:10:41 +0800

    added some custom properties on the button property

--- src/Components/UI/Button.js
+++ src/Components/UI/Button.js
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./Button.module.css";
 
-const Button = () => {
-  return <button></button>;
+const Button = (props) => {
+  return (
+    <button
+      className={styles.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    ></button>
+  );
 };
 
 export default Button;
