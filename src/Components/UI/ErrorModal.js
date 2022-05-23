2e2ce629c0825bc9fc9c219f297a5e30845b5461
Mwau stephen <stephenmwau41@gmail.com>
2022-05-24 01:11:10 +0800

    added a return statement

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -1,23 +1,25 @@
 import React, { Fragment } from "react";
 import Card from "./Card";
 import Button from "./Button";
-import styles from "./ErrorModal.css";
+import styles from "./ErrorModal.module.css";
 
 const ErrorModal = (props) => {
-  <Fragment>
-    <div className={styles.backdrop} />
-    <Card className={styles.modal}>
-      <header className={styles.header}>
-        <h2>{props.title}</h2>
-      </header>
-      <div className={styles.content}>
-        <p>{props.message}</p>
-      </div>
-      <footer className={styles.actions}>
-        <Button>Okay</Button>
-      </footer>
-    </Card>
-  </Fragment>;
+  return (
+    <Fragment>
+      <div className={styles.backdrop} />
+      <Card className={styles.modal}>
+        <header className={styles.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={styles.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button>Okay</Button>
+        </footer>
+      </Card>
+    </Fragment>
+  );
 };
 
 export default ErrorModal;
