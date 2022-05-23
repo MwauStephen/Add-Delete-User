7741aba3dfbb9d7354cc8fab68521c850f9f55ba
Mwau stephen <stephenmwau41@gmail.com>
2022-05-24 00:55:27 +0800

    added contents to the error modal component

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -1,7 +1,22 @@
 import React, { Fragment } from "react";
+import Card from "./Card";
+import Button from "./Button";
 
 const ErrorModal = (props) => {
-  <Fragment></Fragment>;
+  <Fragment>
+    <div />
+    <Card>
+      <header>
+        <h2>{props.title}</h2>
+      </header>
+      <div>
+        <p>{props.message}</p>
+      </div>
+      <footer>
+        <Button>Okay</Button>
+      </footer>
+    </Card>
+  </Fragment>;
 };
 
 export default ErrorModal;
