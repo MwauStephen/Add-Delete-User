f473bc775c4d56f36f9fc6c38718ef04a8c79af1
Mwau stephen <stephenmwau41@gmail.com>
2022-05-23 21:39:09 +0800

    added the class name and props

--- src/Components/UI/Card.js
+++ src/Components/UI/Card.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./Card.module.css";
 
-const Card = () => {
-  return <div>{props.children}</div>;
+const Card = (props) => {
+  return <div className={styles.card}>{props.children}</div>;
 };
 
 export default Card;
